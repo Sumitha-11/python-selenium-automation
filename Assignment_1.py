ddc03db868abe6780b82b0ec84ddc7804f91b68a
sumitha <sundaresansumitha@gmail.com>
2021-11-26 13:33:00 -0800

    Homework1

---
+++ Assignment_1.py
@@ -0,0 +1,9 @@
+1.Amazon Logo                      --->  Search by XPATH , "//i[@class='a-icon a-icon-logo']"
+2.Email Field                      --->  Search by ID , "ap_email"
+3.Continue Button                  --->  Search by XPATH ,"//input[@type='submit']"
+4.Need help link                   --->  Search by XPATH , "//i[@class='a-icon a-icon-expand']" .click()
+5.Forgot your password link        --->  Search by ID , "auth-fpp-link-bottom"
+6.Other issues with Sign-In link   --->  Search by XPATH , "//div[@aria-expanded='true']/a[@id ='ap-other-signin-issues-link']"
+7.Create your Amazon account button--->  Search by ID,"createAccountSubmit"
+8.Conditions of use link           --->  Search by XPATH,"//a[contains(@href,'/gp/help/customer/display.html/ref=ap_desktop_footer_cou?')]"
+9.Privacy Notice link              --->  Search by XPATH , "//a[@class='a-link-normal' and contains(@href,'/gp/help/customer/display.html/ref=ap_desktop_footer_privacy_notice?')]"
