6a825b2d322773f890b42f3e261c6758757729a3
sumitha <sundaresansumitha@gmail.com>
2021-11-26 23:59:05 -0800

    click on orders and returns

---
+++ features/steps/Verify_order.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+
+@given('Open Amazon page')
+def open_google(context):
+    context.driver.get('https://www.Amazon.com')
+@when('Click on Orders and Returns')
+def orders_returns(context):
+    context.driver.find_element(By.XPATH,"//a[@href='/gp/css/order-history?ref_=nav_orders_first']").click()
