4f894dcf9efedfb380b6fb85e5aa6def26fb7f2e
sumitha <sundaresansumitha@gmail.com>
2021-12-04 23:35:06 -0800

    CSS_Selector HW1

---
+++ CSS_Assignment_1.py
@@ -0,0 +1,13 @@
+Amazon Icon --> Search by CSS_SELECTOR, "i[role='img']"
+Create account --> Search by CSS_SELECTOR , "h1.a-spacing-small"
+Your name --> Search by ID, "ap_customer_name"
+Email --> Search by CSS_SELECTOR,"#ap_email"
+Password --> Search by CSS_SELECTOR ,"input[id='ap_password']"
+Password must be atleast 6 characters -->Search by CSS_SELECTOR,"div[class*='a-alert-inline-info']"
+Re-enter password -->Search by CSS_SELECTOR,".a-row input#ap_password_check"
+continue -->Search by CSS_SELECTOR,"input#continue"
+condition of use -->Search by CSS_SELECTOR("a[href*='ref=ap_register_notification_condition_of_use?']")
+Privacy notice --> Search by XPATH("//a[contains(@href,'ref=ap_register_notification_privacy_notice?')]")
+Sign-in -->Search by CSS_SELECTOR("a.a-link-emphasis[href*='/ap/signin?']")
+
+
