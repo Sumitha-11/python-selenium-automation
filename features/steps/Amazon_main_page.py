68e4e10c636149de3120a30b239e0bbe941475a0
sumitha <sundaresansumitha@gmail.com>
2021-12-11 14:18:08 -0800

    HW_4_Assignment_1

--- features/steps/Amazon_main_page.py
+++ features/steps/Amazon_main_page.py
@@ -5,6 +5,12 @@ from time import sleep
 @given('Open Amazon page')
 def open_google(context):
     context.driver.get('https://www.Amazon.com')
+
 @when('Click on Orders and Returns')
 def orders_returns(context):
     context.driver.find_element(By.XPATH,"//a[@href='/gp/css/order-history?ref_=nav_orders_first']").click()
+
+@when('Click on cart icon')
+def Click_cart_icon(context):
+    context.driver.find_element(By.CSS_SELECTOR,"span.nav-cart-icon").click()
+
---
+++ features/tests/Verify_Best_seller_link.feature
@@ -0,0 +1,8 @@
+# Created by USER at 08-12-2021
+Feature: Test scenario for verifying 5 Bestseller links
+
+  Scenario: User should click Bestseller link and verify 5 links are present
+    Given Open Amazon page
+    Then Click on BestSellers Page
+    Then verify 5 links are present
+
