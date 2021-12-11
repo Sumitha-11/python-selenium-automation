d421b65061684809fb71780ab03606bc18ca5438
sumitha <sundaresansumitha@gmail.com>
2021-12-11 14:20:02 -0800

    HW_4_Amazon_main_Page

--- features/steps/Amazon_main_page.py
+++ features/steps/Amazon_main_page.py
@@ -1,6 +1,11 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
+BEST_SELLER_PAGE = (By.CSS_SELECTOR,"a[href*='/gp/bestsellers/']")
+SEARCH_TAB = (By.CSS_SELECTOR,"#twotabsearchtextbox")
+SEARCH_BUTTON=(By.ID,"nav-search-submit-button")
+
+
 
 @given('Open Amazon page')
 def open_google(context):
@@ -13,4 +18,16 @@ def orders_returns(context):
 @when('Click on cart icon')
 def Click_cart_icon(context):
     context.driver.find_element(By.CSS_SELECTOR,"span.nav-cart-icon").click()
+
+@then('Click on BestSellers Page')
+def Click_BestSeller_Page(context):
+    context.driver.find_element(*BEST_SELLER_PAGE).click()
+
+@when('Search for {product}')
+def search_Product(context,product):
+    context.driver.find_element(*SEARCH_TAB).send_keys(product)
+
+@when('Click on search button')
+def Click_search_button(context):
+    context.driver.find_element(*SEARCH_BUTTON).click()
 
