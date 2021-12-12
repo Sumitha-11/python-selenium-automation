cf2278670aafc734c53a6876f4ef3d567e4c6a8b
sumitha <sundaresansumitha@gmail.com>
2021-12-11 17:27:50 -0800

    HW4_2-Product_page

---
+++ features/steps/Product_Page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+import time
+
+FIRST_PRODUCT_LINK = (By.CSS_SELECTOR,".a-size-mini a span")
+ADD_TO_CART_BUTTON = (By.ID,"add-to-cart-button")
+PROTECTION_PLAN = (By.CSS_SELECTOR,"#attachSiNoCoverage")
+
+@then('Click on First product')
+def Click_First_product(context):
+    context.driver.find_element(*FIRST_PRODUCT_LINK).click()
+
+@then('Click on Add to cart Button')
+def Add_Product_cart(context):
+    context.driver.find_element(*ADD_TO_CART_BUTTON).click()
+
+@then('Click on No thanks Button')
+def Click_NO_Thanks(context):
+    context.driver.find_element(*PROTECTION_PLAN).click()
+    time.sleep(4)
