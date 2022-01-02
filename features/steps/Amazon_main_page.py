74efd637e467bbee2e6e79c54133b7a44593b0b2
sumitha <sundaresansumitha@gmail.com>
2022-01-01 18:08:33 -0800

    Amazon main page

--- features/steps/Amazon_main_page.py
+++ features/steps/Amazon_main_page.py
@@ -1,42 +1,49 @@
+import time
+
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 
 BEST_SELLER_PAGE = (By.CSS_SELECTOR,"a[href*='/gp/bestsellers/']")
-SEARCH_TAB = (By.CSS_SELECTOR,"#twotabsearchtextbox")
-SEARCH_BUTTON=(By.ID,"nav-search-submit-button")
+#SEARCH_TAB = (By.CSS_SELECTOR,"#twotabsearchtextbox")
+#SEARCH_ICON=(By.ID,"nav-search-submit-button")
 
 
 @given('Open Amazon page')
-def open_google(context):
-    context.driver.get('https://www.Amazon.com')
+def open_Amazon(context):
+    context.app.main_page.open_page()
+
+
+@when('search for {product}')
+def search_Product(context,product):
+    context.app.header.search_input(product)
+
+
+@when('Click on search button')
+def click_search_button(context):
+    context.app.header.click_search()
 
 
 @when('Click on Orders and Returns')
 def orders_returns(context):
-    context.driver.find_element(By.XPATH,"//a[@href='/gp/css/order-history?ref_=nav_orders_first']").click()
+    context.app.header.click_order()
 
 
 @when('Click on cart icon')
-def Click_cart_icon(context):
-    context.driver.find_element(By.CSS_SELECTOR,"span.nav-cart-icon").click()
+def click_cart_icon(context):
+    context.app.header.click_cart_icon()
 
 
 @then('Click on BestSellers Page')
-def Click_BestSeller_Page(context):
+def click_bestseller_page(context):
     context.driver.find_element(*BEST_SELLER_PAGE).click()
-    context.current_window= context.driver.current_window_handle
-    print(context.current_window)
 
 
-@when('Search for {product}')
-def search_Product(context,product):
-    context.driver.find_element(*SEARCH_TAB).send_keys(product)
+@then('Results for {expected_text} are shown')
+def search_results(context,expected_text):
+    context.app.search_result_page.verify_search_result(expected_text)
 
 
-@when('Click on search button')
-def Click_search_button(context):
-    context.driver.find_element(*SEARCH_BUTTON).click()
 
 
