36d140e7a5c80ca6251767eeaa60bb3307e4712a
sumitha <sundaresansumitha@gmail.com>
2022-01-01 18:09:10 -0800

    verify cart

--- features/steps/Cart_Result_Page.py
+++ features/steps/Cart_Result_Page.py
@@ -1,10 +1,7 @@
 from selenium.webdriver.common.by import By
 from behave import given, when, then
 
-@then('verify your cart is empty')
+
+@then('Verify your cart is empty')
 def verify_cart_empty(context):
-    Expected_Result = context.driver.find_element(By.CSS_SELECTOR, "div[class*='sc-your-amazon-cart-is-empty']").text
-    Actual_Result = "Your Amazon Cart is empty"
-    assert Actual_Result == Expected_Result,f"Actual_result does not match Expected_result"
-    print("Test passed")
-    context.driver.close()
+    context.app.cart_page.cart_page_text()
